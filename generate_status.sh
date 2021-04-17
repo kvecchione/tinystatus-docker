4c829421a9796a5d659e6d61379601b3ce4d7243
Kevin Vecchione <k.vecchione@me.com>
2021-04-17 15:21:18 -0400

    Remove ensure steps

--- generate_status.sh
+++ generate_status.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# ensure config files exist
-if ! [ -f touch /config/checks.csv ];then touch /config/checks.csv;fi
-if ! [ -f touch /config/incidents.txt ];then touch /config/incidents.txt;fi
-
 # generate status page
 sh /srv/tinystatus.sh /config/checks.csv /config/incidents.txt > /web/index.tmp
 
