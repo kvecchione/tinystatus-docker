3261821e934db98d43bfefd1190ca61dbb64f415
Kevin Vecchione <k.vecchione@me.com>
2021-04-16 22:34:07 -0400

    Update generator

--- generate_status.sh
+++ generate_status.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # ensure config files exist
-touch /config/checks.csv /config/incidents.txt
+if [ ! -f touch /config/checks.csv ];then touch /config/checks.csv;fi
+if [ ! -f touch /config/incidents.txt ];then touch /config/incidents.txt;fi
 
 # generate status page
 sh /srv/tinystatus.sh /config/checks.csv /config/incidents.txt > /web/index.tmp
